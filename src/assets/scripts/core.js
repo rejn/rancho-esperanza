d17ff6faf15919c990b76d00ea6a412b5fb256ef
Richard Hallows <hello@richardhallows.com>
2014-11-26 14:13:21 +0000

    Rewrites core.js as ES6, and updates build task accordingly

--- src/assets/scripts/core.js
+++ src/assets/scripts/core.js
@@ -1,39 +1,75 @@
-(function(window, document, undefined) {
+((window, document, undefined) => {
 
   /* global Modernizr */
   'use strict';
+
+  class AddWidgets {
+
+    constructor(window, document, widgetDataAttr) {
+
+      this.window = window;
+      this.document = document;
+      this.widgetDataAttr = widgetDataAttr;
+
+    }
+
+    add() {
+
+      let widgets = document.querySelectorAll(this.widgetDataAttr);
+
+      for (let i = 0; i < widgets.length; ++i) {
+        switch (widgets[i].dataset.widget) {
+          case 'facebook':
+            this.addFacebook(widgets[i]);
+          break;
+          case 'fundrazr':
+            this.addFundrazr();
+          break;
+          case 'gmaps':
+            if (900 < this.window.screen.width) {
+              this.addGmaps(widgets[i]);
+            }
+          break;
+        }
+      }
+
+    }
+
+    addFacebook(el) {
+
+      el.innerHTML = '<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Francho.esperanza.jiquilillo&amp;width=195&amp;height=95&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:195px; height:95px;"></iframe>';
+
+    }
+
+    addFundrazr() {
+
+      let fr = this.document.createElement('script');
+      let head = this.document.querySelector('head');
+      fr.src = 'http://static.fundrazr.com/widgets/loader.js';
+      head.appendChild(fr);
+
+    }
+
+    addGmaps(el) {
+
+      el.innerHTML = '<div class="Embed"><iframe class="Embed-inner" width="100%" height="100%" frameborder="0" src="//maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=+&amp;q=Rancho+Esperanza,+Jiquilillo,+Chinandega,+Nicaragua&amp;ie=UTF8&amp;hq=Rancho+Esperanza,&amp;hnear=Jiquilillo,+Chinandega,+Nicaragua&amp;t=m&amp;cid=7029103330845734291&amp;ll=12.734122,-87.442932&amp;spn=1.607365,1.771545&amp;z=9&amp;iwloc=A&amp;output=embed"></iframe></div>';
+
+    }
+
+  }
 
   // mustard cut?
   if ('querySelector' in document &&
       'addEventListener' in window &&
       Modernizr.dataset) {
 
+    let addWidgets = new AddWidgets(window, document, '[data-widget]');
+
     // on page load
-    window.addEventListener('load', function() {
+    window.addEventListener('load', () => {
 
-      // get nodelists
-      var widgets = document.querySelectorAll('[data-widget]');
-      var head = document.querySelector('head');
+      addWidgets.add();
 
-      // process widgets
-      for (var i = 0; i < widgets.length; ++i) {
-
-        switch (widgets[i].dataset.widget) {
-          case 'facebook':
-            widgets[i].innerHTML = '<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Francho.esperanza.jiquilillo&amp;width=195&amp;height=95&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:195px; height:95px;"></iframe>';
-          break;
-          case 'gmaps':
-            if (900 < window.screen.width) {
-              widgets[i].innerHTML = '<div class="Embed"><iframe class="Embed-inner" width="100%" height="100%" frameborder="0" src="//maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=+&amp;q=Rancho+Esperanza,+Jiquilillo,+Chinandega,+Nicaragua&amp;ie=UTF8&amp;hq=Rancho+Esperanza,&amp;hnear=Jiquilillo,+Chinandega,+Nicaragua&amp;t=m&amp;cid=7029103330845734291&amp;ll=12.734122,-87.442932&amp;spn=1.607365,1.771545&amp;z=9&amp;iwloc=A&amp;output=embed"></iframe></div>';
-            }
-          break;
-          case 'fundrazr':
-            var fr = document.createElement('script');
-            fr.src = 'http://static.fundrazr.com/widgets/loader.js';
-            head.appendChild(fr);
-          break;
-        }
-      }
     }, false);
   }
-}(this, this.document));
+})(this, this.document);
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,6 +6,8 @@ gulp.task('scripts', function() {
 
   var args = require('yargs').argv;
   var gulpIf = require('gulp-if');
+  var sourcemaps = require('gulp-sourcemaps');
+  var to5 = require('gulp-6to5');
   var uglify = require('gulp-uglify');
 
   var paths = {
@@ -16,6 +18,13 @@ gulp.task('scripts', function() {
   var isProduction = args.type === 'production';
 
   return gulp.src(paths.src)
+    .pipe(sourcemaps.init())
+    .pipe(to5())
+    .on('error', function (err) {
+      console.log(err.message);
+      this.emit('end');
+    })
+    .pipe(sourcemaps.write())
     .pipe(gulpIf(isProduction, uglify()))
     .pipe(gulp.dest(paths.dest));
 
